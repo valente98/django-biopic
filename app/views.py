a6fe0282999994b3a31c793a4636076e362c2d81
valente alvarez <valvarez@basecampcodingacademy.org>
2017-12-15 12:54:25 -0600

    change the name of the function and then added a model.

--- app/views.py
+++ app/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from PIL import Image
+from app import models
 
 
 # Create your views here.
-def home(request):
-    return render(request, 'app/base.html')
+def imageInput(request):
+    imageInputModel = models.ImageUploadModel(request.GET)
+    return render(request, 'app/feed.html', {
+        'form': imageInputModel,
+    })
