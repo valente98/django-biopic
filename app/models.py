bf42bcd23d00a59746e6b0b32ed02d4cbd64ceb8
valente alvarez <valvarez@basecampcodingacademy.org>
2017-12-19 11:56:13 -0600

    deleted the class.

--- app/models.py
+++ app/models.py
@@ -1,8 +1,2 @@
 from django.db import models
 from PIL import Image
-
-
-class ImageUploadModel(models.Model):
-    # upload = models.FileField(upload_to='static/app/photos')
-    message = Image.open('app/phot')
-    # image = models.ImageField(upload_to='app/photos/')
