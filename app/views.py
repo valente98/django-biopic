d73663947f8224a1331089acc71a083fa2ff1cad
valente alvarez <valvarez@basecampcodingacademy.org>
2017-12-19 11:56:36 -0600

    change it from models to forms.

--- app/views.py
+++ app/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from PIL import Image
-from app import models
-
+from app import forms
 
 # Create your views here.
+
+
 def imageInput(request):
-    imageInputModel = models.ImageUploadModel(request.GET)
+    imageInputForm = forms.OpenImageFile(request.GET)
     return render(request, 'app/feed.html', {
         'form': imageInputModel,
     })
