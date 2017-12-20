929a69390ef4c8bff0a342b97f8ab88ebefa6302
valente alvarez <valvarez@basecampcodingacademy.org>
2017-12-20 16:07:24 -0600

    change some functions that allows the user see the data they uploaded.

--- app/views.py
+++ app/views.py
@@ -3,6 +3,7 @@ from PIL import Image
 from app import models, forms
 from django.views import View
 from app.forms import ImageUploadForm
+from app.models import ImagePostModel
 
 # Create your views here.
 
@@ -10,15 +11,20 @@ from app.forms import ImageUploadForm
 class UploadImage(View):
     def get(self, request):
         form = ImageUploadForm()
-        return render(request, 'app/feed.html', {
-            'Image': form.Meta.model.image,
-            'caption': form.Meta.model.caption
-        })
+        return render(request, 'app/upload.html', {'form': form})
 
     def post(self, request):
-        form = ImageUploadForm()
+        form = ImageUploadForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
-            return redirect('app:feed')
+            caption = form.cleaned_data['caption']
+            image = form.cleaned_data['image']
+            ImagePostModel(caption=caption, image=image).save()
+            return redirect('app:""')
         else:
             return render(request, 'app/upload.html', {'form': form})
+
+
+class ShowImages(View):
+    def get(self, request):
+        objects = ImagePostModel.objects.all()
+        return render(request, 'app/base.html', {'objects': objects})
