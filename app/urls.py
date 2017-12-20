d55d984c1a46e4bc1e2c96d186e3c58d0bb49105
valente alvarez <valvarez@basecampcodingacademy.org>
2017-12-20 16:26:19 -0600

    change the url from the path and the name.

--- app/urls.py
+++ app/urls.py
@@ -5,5 +5,5 @@ app_name = 'app'
 
 urlpatterns = [
     path('upload/', views.UploadImage.as_view(), name="upload"),
-    path('', views.ShowImages.as_view(), name="feed")
+    path('base', views.ShowImages.as_view(), name="base")
 ]
