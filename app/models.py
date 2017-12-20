90ed00ce75b4b01325fee8da2051b1fafca85d28
valente alvarez <valvarez@basecampcodingacademy.org>
2017-12-20 12:38:01 -0600

    added more inputs to the model then created a function so it would show the images.

--- app/models.py
+++ app/models.py
@@ -5,3 +5,8 @@ from PIL import Image
 class ImagePostModel(models.Model):
     caption = models.CharField(max_length=200)
     image = models.ImageField(upload_to="app/static/app/photos/")
+    uploaded_by = models.CharField(max_length=20, blank=True)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def image_url(self):
+        return self.image.url(len('app/static/'))
