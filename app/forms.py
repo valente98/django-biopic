acc0f885a298fb1b793cd69d8343022ab3b6d258
valente alvarez <valvarez@basecampcodingacademy.org>
2017-12-22 08:33:04 -0600

    Created a class that will help with filtering the image on the site. but its not functional yet.

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from PIL import Image
+from PIL import Image, ImageFilter
 from app.models import ImagePostModel
 
 
@@ -7,3 +7,35 @@ class ImageUploadForm(forms.ModelForm):
     class Meta:
         model = ImagePostModel
         fields = ('uploaded_by', 'image', 'caption')
+
+
+class Filters(forms.Form):
+    choice = {
+        'BLUR': 'Blur',
+        'CONTOUR': 'Contour',
+        'DETAIL': 'Detail',
+        'EDGE_ENHANCE': 'Edge Enhance',
+        'EDGE_ENHANCE_MORE': 'Edge Enhance More',
+        'EMBOSS': 'Emboss',
+        'FIND_EDGES': 'Find Edges',
+        'SMOOTH': 'Smooth',
+        'SMOOTH_MORE': 'Smooth More',
+        'SHARPEN': 'Sharpen'
+    }
+    Filter_Choice = forms.ChoiceField(choices=(choice.items()))
+
+    def filters(self):
+        filter_choice = self.cleaned_data['Filter_choice']
+        filters = {
+            'BLUR': ImageFilter.BLUR,
+            'CONTOUR': ImageFilter.CONTOUR,
+            'DETAIL': ImageFilter.DETAIL,
+            'EDGE_ENHANCE': ImageFilter.EDGE_ENHANCE,
+            'EDGE_ENHANCE_MORE': ImageFilter.EDGE_ENHANCE_MORE,
+            'EMBOSS': ImageFilter.EMBOSS,
+            'FIND_EDGES': ImageFilter.FIND_EDGES,
+            'SMOOTH': ImageFilter.SMOOTH,
+            'SMOOTH_MORE': ImageFilter.SMOOTH_MORE,
+            'SHARPEN': ImageFilter.SHARPEN
+        }
+        return filters[filter_choice]
