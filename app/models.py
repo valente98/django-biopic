a9ec941927165108fb58e84be523ccfda1af745d
valente alvarez <valvarez@basecampcodingacademy.org>
2018-01-19 11:09:23 -0600

    created a class for the user profile. then created a function that will update the profile.

--- app/models.py
+++ app/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from PIL import Image
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class ImagePostModel(models.Model):
@@ -17,3 +20,17 @@ class Comments(models.Model):
     comment = models.CharField(max_length=180)
     time = models.DateTimeField(auto_now_add=True)
     post = models.ForeignKey(ImagePostModel, on_delete=models.CASCADE)
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=500, blank=True)
+    location = models.CharField(max_length=30, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+
+
+@receiver(post_save, sender=User)
+def update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    instance.profile.save()
